9735ccaf337d20e699f73a2d3facf02b2fe7cf51
jfan-dev <122750665+jfan-dev@users.noreply.github.com>
2023-10-05 13:02:01 -0300

    create pokemon card component

---
+++ src/app/components/pkmn-card/pkmn-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pkmn-card',
+  templateUrl: './pkmn-card.component.html',
+  styleUrls: ['./pkmn-card.component.css']
+})
+export class PkmnCardComponent {
+
+}
